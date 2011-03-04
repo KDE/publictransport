7de6359f9ce09b91603eee4aad2d31bebbed8233
Friedrich Karl Tilman Pülz <fpuelz@gmx.de>
2011-03-04 10:53:47 +0100

    Fix pl_pkp: New URL, rewritten parsing script, add route parsing

--- engine/accessorInfos/pl_pkp.js
+++ engine/accessorInfos/pl_pkp.js
@@ -1,7 +1,7 @@
 
 // This function returns a list of all features supported by this script.
 function usedTimetableInformations() {
-	return [ 'StopID' ];
+	return [ 'Platform', 'StopID', 'RouteStops', 'RouteTimes' ];
 }
 
 // This function parses a given HTML document for departure/arrival data.
@@ -10,31 +10,25 @@ function parseTimetable( html ) {
 	if ( html.search(/<td class=\"errormessage\">Your input is ambiguous. Please choose from the selection list.<\/td>/i) != -1 ) {
 		helper.error("No data, only stop suggestions in the HTML document");
 		return false;
-	} else if ( html.search(/<td [^>]*class="errormessage"[^>]*>[^<]*No trains in this space of time[^<]*<\/td>/) != -1 )
-		helper.error("No data for the given space of time in the HTML document");
-		return true; {
+	} else if ( html.search(/<td [^>]*class="errormessage"[^>]*>[^<]*No trains in this space of time[^<]*<\/td>/) != -1 ) {
+		helper.error("No data for the given space of time in the HTML document", html.substr(test, 100));
+		return true;
 	}
-	
-	// Find result table
-	var resultTableRegExp = /<th[^>]*>Time<\/th>[\s\S]*?<\/tr>([\s\S]*)<\/table>/i;
-	var resultTable = resultTableRegExp.exec( html );
-	if ( resultTable == null ) {
-		helper.error("Result table not found!", html);
-		return false;
-	}
-	var str = resultTable[1];
-	
-	// Initialize regular expressions
-	var departuresRegExp = /<tr[^>]*?>([\s\S]*?)<\/tr>/ig;
+
+	// Initialize regula<span class="bold">r expressions
+	var departureRegExp = /<tr class="depboard-(?:dark|light)">([\s\S]*?)<\/tr>/ig;
     var columnsRegExp = /<td[^>]*?>([\s\S]*?)<\/td>/ig;
-	var targetRegExp = /<span class="[^"]*">\s*<a href="[^"]*">\s*([^<]*)\s*<\/a>\s*<\/span>/i;
-	var journeyNewsRegExp = /<span class="rsx">\s*<br>\s*([^<]*)\s*<\/span>/i;
-	var typeOfVehicleRegExp = /<a[^>]*>[\S\s]*<img src="\/img\/([^_]*)_pic.[^"]{3,4}"[^>]*>[\S\s]*<\/a>/i;
-	
-    var timeCol = 0, typeOfVehicleCol = 2, transportLineCol = 4, targetCol = 6, journeyNewsCol = 6;
+	var transportLineRegExp = /<a[^>]*>([\s\S]*?)<\/a>/i;
+	var typeOfVehicleRegExp = /<img src="\/hafas-res\/img\/([^_]*)_pic.gif"[^>]*>/i;
+	var targetRegExp = /<span[^>]*>([\s\S]*)<\/span>/i;
+    var routeBlocksRegExp = /\r?\n\s*(-|&#8226;)\s*\r?\n/gi;
+    var routeBlockEndOfExactRouteMarkerRegExp = /&#8226;/i;
+
+    var timeCol = 0, typeOfVehicleCol = 1, transportLineCol = 1, routeCol = 2, targetCol = 2, platformCol = 3;
 	
 	// Go through all departure blocks
-	while ( (departureRow = departuresRegExp.exec(str)) ) {
+	var departureNumber = 0;
+	while ( (departureRow = departureRegExp.exec(html)) ) {
 		// This gets the current departure row
 		departureRow = departureRow[1];
 		
@@ -45,11 +39,16 @@ function parseTimetable( html ) {
 		}
 		columnsRegExp.lastIndex = 0;
 		
-		if ( columns.length < 7 ) {
-			// Happens very often
-// 			helper.error("Too less columns in a departure row found (" + columns.length + ") " + departureRow);
+		if ( columns.length < 3 ) {
+			if ( departureRow.indexOf("<th") == -1 ) {
+				helper.error("Too lesinterregionals columns in a departure row found (" + columns.length + ") ", departureRow);
+			}
 			continue;
 		}
+		
+		// Initialize result variables with defaults
+		var time = 0, typeOfVehicle = "", transportLine = "", targetString = "", platformString = "",
+			routeStops = new Array, routeTimes = new Array, exactRouteStops = 0;
 		
 		// Parse time column
 		time = helper.matchTime( helper.trim(helper.stripTags(columns[timeCol])), "hh:mm" );
@@ -58,58 +57,95 @@ function parseTimetable( html ) {
 			continue;
 		}
 		
-		// Parse type of vehicle column
-		var typeOfVehicle = typeOfVehicleRegExp.exec(columns[typeOfVehicleCol]);
-		if ( typeOfVehicle != null ) {
-			typeOfVehicle = typeOfVehicle[1];
-			
-			var vehicle = typeOfVehicle.toLowerCase();
-			// TODO: Check if these are the right train type:
-			if ( vehicle == "dpn" || vehicle == "t84" || vehicle == "r84" ) {
-				typeOfVehicle = "regional";
-			}
+		if ( (typeOfVehicle = typeOfVehicleRegExp.exec(columns[typeOfVehicleCol])) ) {
+			typeOfVehicle = typeOfVehicle[1].toLowerCase();
 		} else {
 			helper.error("Unexpected string in type of vehicle column", columns[typeOfVehicleCol]);
-			typeOfVehicle = "Unknown";
+			continue;
+		}
+		
+		if ( typeOfVehicle == "skw" || typeOfVehicle == "wkd" || typeOfVehicle == "km" ) {
+			typeOfVehicle = "regional";
+		} else if ( typeOfVehicle == "tlk" ) {
+			typeOfVehicle = "interregional";
+		} else if ( typeOfVehicle == "eic" || typeOfVehicle == "ex" ) {
+			typeOfVehicle = "intercity";
+		} else if ( typeOfVehicle == "trm" ) {
+			typeOfVehicle = "tram";
+		}
+		
+		// Parse transport line column
+		if ( (transportLine = transportLineRegExp.exec(columns[transportLineCol])) ) {
+			transportLine = helper.trim(helper.stripTags(transportLine[1]));
+		} else {
+			helper.error("Unexpected string in transport line column", columns[transportLineCol]);
+			continue;
 		}
 		
 		// Parse target column
-		var targetString = targetRegExp.exec(columns[targetCol]);
-		if ( targetString != null ) {
-			targetString = targetString[1];
+		if ( (targetString = targetRegExp.exec(columns[targetCol])) ) {
+			targetString = helper.trim( helper.stripTags(targetString[1]) );
 		} else {
 			helper.error("Unexpected string in target column", columns[targetCol]);
 			continue;
 		}
 		
-		var journeyNewsOther = journeyNewsRegExp.exec(columns[targetCol]);
-		if ( journeyNewsOther != null ) {
-			journeyNewsOther = journeyNewsOther[1];
+		// Parse route column
+		var route = columns[routeCol];
+		var posRoute = route.indexOf( "<br>" );
+		if ( posRoute != -1 ) {
+			route = route.substr( posRoute + 6 );
+			var routeBlocks = route.split( routeBlocksRegExp );
+
+			if ( !routeBlockEndOfExactRouteMarkerRegExp.test(route) ) {
+				exactRouteStops = routeBlocks.length;
+			} else {
+				while ( (splitter = routeBlocksRegExp.exec(route)) ) {
+					++exactRouteStops;
+					if ( routeBlockEndOfExactRouteMarkerRegExp.test(splitter) )
+						break;
+				}
+			}
+			
+			for ( var n = 0; n < routeBlocks.length; ++n ) {
+				var lines = helper.splitSkipEmptyParts( routeBlocks[n], "\n" );
+				if ( lines.count < 4 )
+					continue;
+
+				routeStops.push( helper.trim(helper.stripTags(lines[1])) );
+				routeTimes.push( lines[3] );
+			}
 		}
 		
-		var transportLine = helper.trim( helper.stripTags(columns[transportLineCol]) );
+		// Parse platform column
+		var platformString = columns.length > platformCol && typeof(columns[platformCol]) != 'undefined'
+				? helper.trim( helper.stripTags(columns[platformCol]) ) : "";
 		
 		// Add departure to the result set
-		// TODO: Fill in parsed values instead of the sample strings.
-		// You can also add other information, use the code completion
-		// (Ctrl+Space) for more information.
 		timetableData.clear();
 		timetableData.set( 'DepartureHour', time[0] );
 		timetableData.set( 'DepartureMinute', time[1] );
 		timetableData.set( 'TransportLine', transportLine );
 		timetableData.set( 'TypeOfVehicle', typeOfVehicle );
-		if ( journeyNewsOther != null ) {
-			timetableData.set( 'JourneyNewsOther', journeyNewsOther );
-		}
+		timetableData.set( 'Platform', platformString );
 		timetableData.set( 'Target', targetString );
+		timetableData.set( 'RouteStops', routeStops );
+		timetableData.set( 'RouteTimes', routeTimes );
+		timetableData.set( 'RouteExactStops', exactRouteStops );
 		result.addData( timetableData );
+		
+		++departureNumber;
+	}
+	
+	if ( departureNumber == 0 ) {
+		helper.error( "Regexp didn't match anything", html );
 	}
 }
 
 // This function parses a given HTML document for stop suggestions.
 function parsePossibleStops( html ) {
     // Find block of stops
-    var stopRangeRegExp = /<td class="result"[^>]*>\s*<img[^>]*>\s*<select name="input"[^>]*>([\s\S]*?)<\/select>/i;
+    var stopRangeRegExp = /<select class="error" name="input"[^>]*>([\s\S]*?)<\/select>/i;
 	var range = stopRangeRegExp.exec( html );
 	if ( range == null ) {
 		helper.error( "Stop range not found", html );
