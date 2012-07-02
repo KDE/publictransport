b203aaff0d2590ccd9e6ad700e859545dcf8ff64
Friedrich Karl Tilman Pülz <fpuelz@gmx.de>
2012-07-02 14:33:41 +0200

    Fix de_dvb provider
    - Update to new Script API
    - Fix stop suggestion URL
    - Parse StopCity for stop suggestions

--- engine/script/serviceProviders/de_dvb.js
+++ engine/script/serviceProviders/de_dvb.js
@@ -25,12 +25,15 @@ function parseTimetable( json ) {
     while ( (departure = departureRegExp.exec(json)) ) {
         var minutes = parseInt( departure[3] );
         var timeString = helper.addMinsToTime( now.getHours() + ":" + now.getMinutes(), minutes,
-                                        "h:mm" );
-        var time = helper.matchTime( timeString, "hh:mm" );
-        if ( time.length != 2 ) {
+                                        "h:m" );
+        var time = helper.matchTime( timeString, "h:m" );
+        if ( time.error ) {
                 helper.error("Unexpected string in time column!", departure[3]);
                 continue;
         }
+        var dateTime = new Date();
+	dateTime.setHours( time.hour );
+	dateTime.setMinutes( time.minute );
 
         var vehicleType = "Unknown";
         var line = departure[1];
@@ -44,45 +47,36 @@ function parseTimetable( json ) {
         }
 
         // Add departure
-        timetableData.clear();
-        timetableData.set( 'TransportLine', departure[1] );
-        timetableData.set( 'Target', departure[2] );
-        timetableData.set( 'TypeOfVehicle', vehicleType );
-        timetableData.set( 'DepartureHour', time[0] );
-        timetableData.set( 'DepartureMinute', time[1] );
-        result.addData( timetableData );
+        result.addData( {TransportLine: departure[1],
+			 Target: departure[2],
+			 TypeOfVehicle: vehicleType,
+			 DepartureDateTime: dateTime} );
     }
 }
 
 function getStopSuggestions( values  ) {
     var timestamp = Math.round( (new Date()).getTime() / 1000 ); // in seconds since 1970-01-01...
-    var url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=" + values.stop
-            + "&vz=0&time=" + timestamp + "&iso=1";
+    var url = "http://widgets.vvo-online.de/abfahrtsmonitor/Haltestelle.do?hst=" + values.stop
+            + "?&vz=0&time=" + timestamp + "&iso=1";
     var json = network.getSynchronous( url );
 
     if ( !network.lastDownloadAborted ) {
         // Initialize regular expressions (compile them only once)
         var stopRangeRegExp = /\[\[\["[^"]*"\]\],\[(.*)\]\]$/i;
-        var stopRegExp = /\["([^"]*)","[^"]*","([^"]*)"\]/ig;
+        var stopRegExp = /\["([^"]*)","([^"]*)","([^"]*)"\]/ig;
 
         // Get range of all stops
         var range = stopRangeRegExp.exec( json );
         if ( range == null ) {
-                helper.error("Stop range not found", json);
-                return false;
+	    helper.error("Stop range not found", json);
+	    return false;
         }
         range = range[1];
 
         // Go through all stops
         while ( (stop = stopRegExp.exec(range)) ) {
-            var stopName = stop[1];
-            var stopID = stop[2];
-
             // Add stop
-            timetableData.clear();
-            timetableData.set( 'StopName', stopName );
-            timetableData.set( 'StopID', stopID );
-            result.addData( timetableData );
+            result.addData( {StopName: stop[1], StopCity: stop[2], StopID: stop[3]} );
         }
     }
 
